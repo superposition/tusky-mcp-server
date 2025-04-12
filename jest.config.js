8c0a70fbfce56a85738581127bf6f8241a787be2
Eric Manganaro <ericmanganaro@gmail.com>
2025-04-11 23:10:21 -0400

    AUTH-1: Add Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
